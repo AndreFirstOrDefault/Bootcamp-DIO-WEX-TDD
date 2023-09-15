aa0284e50d1af67c901ef938549a13c43aea04e1
andrejava2022 <andre.java.ads.2022@gmail.com>
2023-09-15 18:26:09 -0300

    Adicionar arquivos de projeto.

---
+++ testeNewTalents/UnitTest1.cs
@@ -0,0 +1,15 @@
+using New_Talents;
+using System;
+using Xunit;
+
+namespace testeNewTalents
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calc = new Calculadora();
+        }
+    }
+}
